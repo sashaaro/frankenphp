f96c86f1a7856e7e7e0dffe92197f049102d324d
Aleksandr Arofikin <sashaaro@gmail.com>
2022-11-30 15:07:51 +0600

    httpclient raw request

--- testdata/client-send-request.php
+++ testdata/client-send-request.php
@@ -3,6 +3,8 @@
 require_once __DIR__.'/_executor.php';
 
 return function () {
-    echo frankenphp_client_send_request("GET /headers HTTP/1.1
+  // echo frankenphp_client_send_request("https://httpbin.org/headers");
+
+  echo frankenphp_client_send_request("GET /headers HTTP/1.1
 Host: httpbin.org");
 };
